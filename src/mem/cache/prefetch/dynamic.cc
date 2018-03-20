8a1d92fb987a6929027960329c7e1ef0320d7c2f
Matthias Guenther <mrguenther@email.arizona.edu>
2018-03-20 14:52:41 -0700

    Created dynamic.cc and dynamic.hh in mem/cache/prefetch/

---
+++ src/mem/cache/prefetch/dynamic.cc
@@ -0,0 +1,3 @@
+
+#include "mem/cache/prefetch/dynamic.hh"
+
---
+++ src/mem/cache/prefetch/dynamic.hh
@@ -0,0 +1,12 @@
+#ifndef __MEM_CACHE_PREFETCH_BASE_HH__
+#define __MEM_CACHE_PREFETCH_BASE_HH__
+
+#include "mem/cache/prefetch/base.hh"
+
+
+
+
+
+
+
+#endif //__MEM_CACHE_PREFETCH_BASE_HH__
