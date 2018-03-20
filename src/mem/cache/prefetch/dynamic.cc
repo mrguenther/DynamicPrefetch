7f64bb23f8e8a6849c3206e939be45614799f299
Matthias Guenther <mrguenther@email.arizona.edu>
2018-03-20 15:56:23 -0700

    Added PrefetchConfig class nested within DynamicPrefetcher.

--- src/mem/cache/prefetch/dynamic.cc
+++ src/mem/cache/prefetch/dynamic.cc
@@ -1,3 +1,8 @@
-
 #include "mem/cache/prefetch/dynamic.hh"
 
+DynamicPrefetcher::DynamicPrefetcher(const DynamicPrefetcherParams* p)
+	: BasePrefetcher(p) /*
+	, nextParameter(p->nextParameter)
+	, etc(p->etc) */
+{
+}
